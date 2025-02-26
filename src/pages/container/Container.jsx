476bd6bd2131e9cd5ffa9be8ba9649d484e37c55
Leon-kings <leonakingeneye2002@gmail.com>
2025-02-25 18:17:51 -0800

    changes

--- src/pages/container/Container.jsx
+++ src/pages/container/Container.jsx
@@ -7,7 +7,7 @@ export default function Container() {
       <div className="w-full bg-black">
         <div className="text-center text-white pt-4 pb-8">
           <h2 className="text-3xl font-bold leading-tight text-whitesm:text-4xl xl:text-5xl font-pj">
-           SKILLLS 
+            SKILLLS
           </h2>
           <p className="mt-4 text-base leading-7 text-white sm:mt-8 font-pj">
             Here are some Skills we can serve it through . Managed, secure &
@@ -24,9 +24,8 @@ export default function Container() {
                       Languages and Structal Languages
                     </h1>
                     <p className="mt-4 text-lg text-white">
-                      This year, our new summer collection will shelter you the
-                      best magnificient Computers To advance technology Here in
-                      Rwanda.
+                      I&apos;m a self-motivated and hardworking individual who is
+                      always ready to learn new things and work in a team.
                     </p>
                     <br />
                     <p className="mt-4 text-lg text-white">
@@ -43,33 +42,13 @@ export default function Container() {
                             <div className="grid shrink-0 grid-cols-1 ">
                               <div className="h-44 w-40 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
                                 <img
-                                  src='https://web-design-eastbourne.co.uk/News/wp-content/uploads/2023/11/java.jpg'
+                                  src="https://web-design-eastbourne.co.uk/News/wp-content/uploads/2023/11/java.jpg"
                                   className="size-fit object-cover"
                                 />
                               </div>
                               <div className="h-44 w-40 overflow-hidden rounded-lg">
                                 <img
-                                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PwRQttmK18ZBYWADrccj8tjjsnIWtDFvqQ&s'
-                                  className="size-fit object-cover"
-                                />
-                              </div>
-                            </div>
-                            <div className="grid shrink-0 grid-cols-1 ">
-                              <div className="h-44 w-40 overflow-hidden rounded-lg">
-                                <img
-                                  src='https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/4085715/cover_image/regular_1708x683/0925-3D_Data_Visualization_with_Open_Source_Tools_A_Tutorial_Using_VTK_Dan_Newsletter-8de886e8cfe7817dd6f2502c43aacdce.png'
-                                  className="size-fit object-cover"
-                                />
-                              </div>
-                              <div className="h-44 w-40 overflow-hidden rounded-lg">
-                                <img
-                                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStJY57MnPNeUz2cVItQ-QJlp_oo_UlkUwwEA&s'
-                                  className="size-fit object-cover"
-                                />
-                              </div>
-                              <div className="h-44 w-40 overflow-hidden rounded-lg">
-                                <img
-                                  src='https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg'
+                                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PwRQttmK18ZBYWADrccj8tjjsnIWtDFvqQ&s"
                                   className="size-fit object-cover"
                                 />
                               </div>
@@ -77,7 +56,27 @@ export default function Container() {
                             <div className="grid shrink-0 grid-cols-1 ">
                               <div className="h-44 w-40 overflow-hidden rounded-lg">
                                 <img
-                                  src='https://cdn.educba.com/academy/wp-content/uploads/2019/06/What-is-Sass-2.jpg'
+                                  src="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/4085715/cover_image/regular_1708x683/0925-3D_Data_Visualization_with_Open_Source_Tools_A_Tutorial_Using_VTK_Dan_Newsletter-8de886e8cfe7817dd6f2502c43aacdce.png"
+                                  className="size-fit object-cover"
+                                />
+                              </div>
+                              <div className="h-44 w-40 overflow-hidden rounded-lg">
+                                <img
+                                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStJY57MnPNeUz2cVItQ-QJlp_oo_UlkUwwEA&s"
+                                  className="size-fit object-cover"
+                                />
+                              </div>
+                              <div className="h-44 w-40 overflow-hidden rounded-lg">
+                                <img
+                                  src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg"
+                                  className="size-fit object-cover"
+                                />
+                              </div>
+                            </div>
+                            <div className="grid shrink-0 grid-cols-1 ">
+                              <div className="h-44 w-40 overflow-hidden rounded-lg">
+                                <img
+                                  src="https://cdn.educba.com/academy/wp-content/uploads/2019/06/What-is-Sass-2.jpg"
                                   className="size-fit object-cover"
                                 />
                               </div>
